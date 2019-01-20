2c9d3d98dd6428c877ce3d69d197ef0ee6ed5bf5
Christian <webmaster@cs-networks.info>
2019-01-20 23:46:57 +0100

    Multi client

--- test/de/bht/puf/cs/client/test/TestNetClient.java
+++ test/de/bht/puf/cs/client/test/TestNetClient.java
@@ -2,44 +2,71 @@ package de.bht.puf.cs.client.test;
 
 import de.bht.puf.cs.Score;
 import de.bht.puf.cs.client.NetClient;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 /**
  * JUnit Tests for NetClient
  * 
+ * @since JUnit 4
  * @version 1.0
  * @author Christian Schroeter
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestNetClient {
     
-    public TestNetClient() {
+    //params for client Score
+    String name;
+    int score;
+    String time;
+    
+    static NetClient client;
+    
+    public TestNetClient(String name, int score, String time) {
+        this.name = name;
+        this.score = score;
+        this.time = time;
     }
-
+    
+    @BeforeClass
+    public static void Test0_setUp() {
+        client = new NetClient();
+    }
     
     /**
-     * Test to send a new Score to NetServer in both valid options
+     * Test to send a new Score to NetServer with params
      */
     @Test
-    public void TestSendScore () {
-        String name = "Player";
-        int score = 300;
-        String time = "0:05:25";
-        
-        assertTrue(NetClient.sendScore(name, score, time));
-        score = 310;
-        assertTrue(NetClient.sendScore((new Score(name, score, time))));
+    public void Test1_sendScoreWithParams () {
+        assertTrue(client.sendScore(name, score, time));
+    }
+    
+    /**
+     * Test to send a new Score to NetServer as Score
+     */
+    @Test
+    public void Test2_sendScore () {
+        assertTrue(client.sendScore((new Score(name, score, time))));
     }
     
     /**
      * Test to request HighSCore from NetServer
      */
     @Test
-    public void TestGetHighscore () {
+    public void Test3_getHighscore () {
         String hs;
         
-        hs = NetClient.getHighscore();
+        hs = client.getHighscore();
         assertTrue(!hs.isEmpty());
-        System.out.print(hs);
+        System.out.print(hs + "\n\n");
+    }
+    
+    @AfterClass
+    public static void Test4_cleanUp () {
+        assertTrue(client.closeSession());
     }
 }
